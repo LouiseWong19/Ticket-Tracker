4cf1d831cb82d0e89b93a106f482a3996930d2b6
Louise Wong <lw17122@bristol.ac.uk>
2021-12-02 13:48:47 +0000

    added useState -> fixed counter

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import react, {useState} from 'react';
 import './Counter.scss';
 
-const counter = () =>{
-  return <div className = "counter">
+const Counter = () =>{
+  const [counter,setCounter] = useState(0);
+
+  const handleIncrement = () =>{
+    setCounter(counter + 1);
+  }
+
+  const handleDecrement = () =>{
+    if(counter === 0){
+      setCounter(0)
+    }else {
+      setCounter(counter - 1);
+    }
+  }
+
+  return (<div className = "counter">
     <h4>Counter</h4>
-    <h4>0</h4>
+    <h4>{counter}</h4>
     <div className="counter__button">
-      <button>-</button>
-      <button>+</button>
+      <button onClick={handleDecrement}>-</button>
+      <button onClick={handleIncrement}>+</button>
     </div>
   </div>
-}
+  )}
 
-export default counter;
+export default Counter;
