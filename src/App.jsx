cb363806f645092715e382d329992a4dfa1a9493
Louise Wong <lw17122@bristol.ac.uk>
2021-12-02 14:34:07 +0000

    second trial

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,7 @@ import EmployeeTile from "./Components/Employee/Employee";
 
 
 const App = () => {
-  const employeeNames = team.map((names)=>{
-    return names.name;
-  });
-  console.log(employeeNames)
-
-  const employeeRoles = team.map((roles)=>{
-    return roles.role;
-  });
-  console.log(employeeRoles)
-
+  
   return (
     <div className="app">
       <nav className="nav">
